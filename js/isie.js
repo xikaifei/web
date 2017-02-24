0011fa57a3fde3011aabb14b2bc8ad88a66eb19c
xikaifei <kaifei.xi@pactera.com>
2017-02-24 18:14:03 +0800

    .

---
+++ js/isie.js
@@ -0,0 +1,4 @@
+if(window.attachEvent){
+    document.body.innerHTML="";
+    alert("还在用IE？慢走不送~");
+}
--- js/public.js
+++
@@ -1,7 +0,0 @@
-if(window.attachEvent){
-    document.body.innerHTML="";
-    alert("还在用低版本IE？慢走不送~");
-}else{
-    document.body.innerHTML="";
-    alert("敢不敢换个正常的浏览器？")
-}
